9a8e3c3a0a1f77e633733539f5bd05f8793c26a2
Sergey Gusev <serpentano@gmail.com>
2019-11-16 16:13:41 +0300

    added saving candidate resource

--- resources/candidate/Candidate.js
+++ resources/candidate/Candidate.js
@@ -5,10 +5,23 @@ var CandidateSchema = mongoose.Schema({
     name: {
         type: String,
         required: true
-    }
+    },
+    description: {
+        type: String,
+        required: false
+    },
+
 });
 // Export Contact model
 var Candidate = module.exports = mongoose.model('candidates', CandidateSchema);
-module.exports.get = function (callback, limit) {
-    Candidate.find(callback).limit(limit);
+
+module.exports.index = async function () {
+    return Candidate.find();
+}
+
+module.exports.create = async function(data) {
+    
+    const candidate = new Candidate(data);
+    return candidate.save()
+
 }
--- resources/candidate/CandidateController.js
+++ resources/candidate/CandidateController.js
@@ -3,21 +3,26 @@
 var Candidate = require("./Candidate");
 
 module.exports = {
-    index: function (req, res) {
-        Candidate.get(function (err, candidates) {
-            if (err) {
-                res.json({
-                    status: "error",
-                    message: err,
-                });
-            }
-            res.json(
-                candidates
-            );
-        });
+    index: async function (req, res) {
+        try {
+            const candidates = await Candidate.index();
+
+            res.json(candidates);
+        }
+        catch(err) {
+            res.status(500).json({ message: err.message});
+        }
     },
-    create: function (req, resp) {
-        //console.dir(req);
+    create: async function (req, res) {
+        try {
+            const candidate = await Candidate.create(req.body);
+
+            res.status(201).json(candidate);
+        }
+        catch(err) {
+            console.dir(err);
+            res.status(500).json({ message: err.message});
+        }
     }
 
 }
